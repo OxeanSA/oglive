bd1d16e0eb8da4cb6f332538e504c7877f4a8160
OxeanSA <oxeansa@gmail.com>
2025-03-25 12:41:26 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,17 +6,17 @@ import os
 
 port = "5000"
 server_ip = get_ip()
-app = create_app()
+#app = create_app()
 
 if __name__ == '__main__':
 	os.system('clear')
 	print("V2 API Server")
-
-	app.listen(port=int(port), address=server_ip)
+	create_app()
+#	app.listen(port=int(port), address=server_ip)
 	time.sleep(1)
 	print("IP: " + server_ip)
 	time.sleep(1)
 	print("Port: " + port)
 	time.sleep(1)
 	print("Online •")
-	IOLoop.instance().start()
+#	IOLoop.instance().start()
